65d20a80b6e2d1161c1da5eafe9f74f2f86aa38a
shogo4131 <shougo08697118@gmail.com>
2022-07-05 23:59:49 +0900

    feat: add basic auth

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from "next/server";
+import { Buffer } from "buffer";
+
+export function middleware(req: NextRequest) {
+  const basicAuth = req.headers.get("authorization");
+
+  if (basicAuth) {
+    const auth = basicAuth.split(" ")[1];
+    const [user, pwd] = Buffer.from(auth, "base64").toString().split(":");
+
+    if (
+      user === process.env.NEXT_PUBLIC_USER &&
+      pwd === process.env.NEXT_PUBLIC_PASS
+    ) {
+      return NextResponse.next();
+    }
+  }
+
+  return NextResponse.next({
+    status: 401,
+    headers: {
+      "WWW-Authenticate": "Basic realm=realm",
+    },
+  });
+}
